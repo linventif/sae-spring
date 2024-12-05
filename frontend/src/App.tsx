046aa92aa87f60ba283ba3be736c3e1dc0c486eb
linventif <linventif@gmail.com>
2024-12-05 17:15:46 +0100

    Init: add solidJS frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,30 @@
+import type { Component } from 'solid-js';
+
+import logo from './logo.svg';
+import styles from './App.module.css';
+
+const App: Component = () => {
+  //   fetch localhost:8080/hello and log the response
+    fetch('http://localhost:8080/hello')
+        .then(console.log)
+  return (
+    <div class={styles.App}>
+      <header class={styles.header}>
+        <img src={logo} class={styles.logo} alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          class={styles.link}
+          href="https://github.com/solidjs/solid"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Solid
+        </a>
+      </header>
+    </div>
+  );
+};
+
+export default App;
